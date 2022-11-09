b077a9a5f325d58ebce2059d5cc84b1c76459edc
Lili Hovsepyan <lilllihovsepyan@gmail.com>
2022-11-09 15:27:51 +0400

    Still didn't deployed

---
+++ mobile-ultrasound-main/js/script.js
@@ -0,0 +1,25 @@
+(() => {
+    const menu = document.querySelector('header .inner #menu-bar');
+    const navbar = document.querySelector('header .inner nav');
+    const navLinks = document.querySelectorAll('.navbar li');
+
+    menu.addEventListener('click', openToggleFunc);
+
+    function openToggleFunc() {
+        navbar.classList.toggle('active');
+    }
+
+    if (navLinks.length) {
+        navLinks.forEach((link) => {
+            link.addEventListener('click', (e) => {
+                navLinks.forEach((link) => {
+                    link.classList.remove('active');
+                });
+                link.classList.add('active');
+            });
+        });
+    }
+
+    const date = new Date();
+    document.getElementById('currentYear').innerHTML = date.getFullYear();
+})();
